b561c002041371e682287b0075729b726174aad5
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2022-12-08 21:34:05 -0300

    Update 1002.js

--- Iniciante/1002.js
+++ Iniciante/1002.js
@@ -1,7 +1,5 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
 
-const raio = Number(lines[0])
+const raio = 2
 const n = 3.14159
 
 const area=n*(raio**2)
